576e18ff5ac3dad9c3e52d6aacfbe3c033611c93
mihran-w <mihranw@gmail.com>
2022-03-08 20:04:08 +0330

    New Sample Work

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,34 @@
 // Ityped
 var txtType = document.getElementById("aboutTyped");
-ityped.init(txtType, {showCursor : true, strings: ['برنامه نویس و طراح سایت', 'مدرس دوره های برنامه نویسی' , 'مشاور پروژه' ] });
-
+ityped.init(txtType, {
+  showCursor: true,
+  strings: [
+    "برنامه نویس و طراح سایت",
+    "مدرس دوره های برنامه نویسی",
+    "مشاور پروژه",
+  ],
+});
 
 // Dark Mode Theme
 $(document).ready(function () {
-
   var ThemeVal = localStorage.getItem("ThemeMode");
-  if(ThemeVal == "dark"){
+  if (ThemeVal == "dark") {
     $("html").addClass("dark");
-    $("#swBtn").attr("checked","checked");
-  }else{
+    $("#swBtn").attr("checked", "checked");
+  } else {
     $("html").removeClass("dark");
   }
 
-
-  $("#swBtn").click(function () { 
-    if($(this).is(":checked")){
+  $("#swBtn").click(function () {
+    if ($(this).is(":checked")) {
       $("html").addClass("dark");
-      localStorage.setItem("ThemeMode","dark");
-    }else{
+      localStorage.setItem("ThemeMode", "dark");
+    } else {
       $("html").removeClass("dark");
-      localStorage.setItem("ThemeMode","light");
+      localStorage.setItem("ThemeMode", "light");
     }
   });
 });
-
-
 
 // Prue Counter init
 $(document).ready(function () {
@@ -34,14 +36,21 @@ $(document).ready(function () {
 });
 
 // Preloader
-// $(window).on("load",function(){
-//   $(".preloader").fadeOut("slow");
-// })
+$(window).on("load", function () {
+  $(".preloader").fadeOut(1000);
+});
 
-// Preloader
-// $(window).on("load", function(){
-//   $(".preloader").delay(800).fadeOut("slow");
-// })
-
+// Owl Carousel
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 4,
+    loop: true,
+    margin: 4,
+    dotsEach: true,
+    lazyLoad: true,
+    autoplay: true,
+    animateIn:true
+  });
+});
 
 
